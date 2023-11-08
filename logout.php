399be299723d137b6721cc2d78141d0a7c267715
Revellin Mendesz <99839050+revellin@users.noreply.github.com>
2023-11-08 09:23:44 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,9 @@
 <?php
-
-session_destroy();
-
+session_start();
+ob_start();
+unset($_SESSION['id'], $_SESSION['nome']);
+$_SESSION['msg'] = "<p style='color: green'>Deslogado com sucesso!</p>";
+ 
+header("Location: index.php");
+ 
 ?>
