0b35357e38e7c28856a030cdabfc2a06d4eb6b33
Luisa <126588980+LuisaSantosSilva@users.noreply.github.com>
2023-11-13 19:04:56 -0300

    Delete logout.php
    Deletando arquivos e pastas que não fazem parte da main

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-ob_start();
-unset($_SESSION['id'], $_SESSION['nome']);
-$_SESSION['msg'] = "<p style='color: green'>Deslogado com sucesso!</p>";
- 
-header("Location: index.php");
- 
-?>
