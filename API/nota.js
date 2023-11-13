8cf741c48b219fb3eebc550bd792a8db72f7dd3f
Luisa <126588980+LuisaSantosSilva@users.noreply.github.com>
2023-11-13 19:10:38 -0300

    Adicionando API
    Fazendo update das APIs

---
+++ API/nota.js
@@ -0,0 +1,23 @@
+const allStar = document.querySelectorAll('.rating .star')
+const ratingValue = document.querySelector('.rating input')
+
+allStar.forEach((item, idx)=> {
+	item.addEventListener('click', function () {
+		let click = 0
+		ratingValue.value = idx + 1
+
+		allStar.forEach(i=> {
+			i.classList.replace('bxs-star', 'bx-star')
+			i.classList.remove('active')
+		})
+		for(let i=0; i<allStar.length; i++) {
+			if(i <= idx) {
+				allStar[i].classList.replace('bx-star', 'bxs-star')
+				allStar[i].classList.add('active')
+			} else {
+				allStar[i].style.setProperty('--i', click)
+				click++
+			}
+		}
+	})
+})
